de883d1959babbb7ad66c0d1feaddc9068742985
MadInArt <knyazvladimir26@gmail.com>
2020-01-20 18:23:21 +0100

    Updates

---
+++ precache-manifest.072ebad8a3b69bd0f4e91c0313be7259.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ca326ed539be155c8239dee009a330d",
+    "url": "/movies-app/index.html"
+  },
+  {
+    "revision": "1fa15ae9458db74ee611",
+    "url": "/movies-app/static/css/main.77292d91.chunk.css"
+  },
+  {
+    "revision": "38ddfbdabab80643e21e",
+    "url": "/movies-app/static/js/2.03456b18.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/movies-app/static/js/2.03456b18.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1fa15ae9458db74ee611",
+    "url": "/movies-app/static/js/main.1c2c775c.chunk.js"
+  },
+  {
+    "revision": "fb00f3fd37145ea2a76a",
+    "url": "/movies-app/static/js/runtime-main.bfa88d98.js"
+  }
+]);
